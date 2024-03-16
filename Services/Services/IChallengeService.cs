58773eb655ef50b092918390a77458e86fb209e3
DrTau <drttau@gmail.com>
2024-03-16 22:17:35 +0300

    Add comments

--- Services/Services/IChallengeService.cs
+++ Services/Services/IChallengeService.cs
@@ -2,10 +2,36 @@ using Challengify.Models;
 
 namespace Challengify.Services;
 
+/// <summary>
+/// Represents a service for managing challenges.
+/// </summary>
 public interface IChallengeService
 {
+    /// <summary>
+    /// Creates a new challenge.
+    /// </summary>
+    /// <param name="challenge">The challenge to create.</param>
+    /// <returns>The created challenge.</returns>
     public Task<Challenge> CreateChallengeAsync(Challenge challenge);
+
+    /// <summary>
+    /// Retrieves a challenge by its ID.
+    /// </summary>
+    /// <param name="challengeId">The ID of the challenge to retrieve.</param>
+    /// <returns>The retrieved challenge.</returns>
     public Task<Challenge> GetChallengeAsync(long challengeId);
+
+    /// <summary>
+    /// Updates an existing challenge.
+    /// </summary>
+    /// <param name="challenge">The challenge to update.</param>
+    /// <returns>The updated challenge.</returns>
     public Task<Challenge> UpdateChallengeAsync(Challenge challenge);
+
+    /// <summary>
+    /// Deletes a challenge by its ID.
+    /// </summary>
+    /// <param name="challengeId">The ID of the challenge to delete.</param>
+    /// <returns>The deleted challenge.</returns>
     public Task<Challenge> DeleteChallengeAsync(long challengeId);
 }
--- Services/Services/IResultService.cs
+++ Services/Services/IResultService.cs
@@ -2,10 +2,36 @@ using Challengify.Models;
 
 namespace Challengify.Services;
 
+/// <summary>
+/// Represents a service for managing results.
+/// </summary>
 public interface IResultService
 {
+    /// <summary>
+    /// Creates a new result asynchronously.
+    /// </summary>
+    /// <param name="result">The result to create.</param>
+    /// <returns>The created result.</returns>
     public Task<Result> CreateResultAsync(Result result);
+
+    /// <summary>
+    /// Retrieves a result by its ID asynchronously.
+    /// </summary>
+    /// <param name="resultId">The ID of the result to retrieve.</param>
+    /// <returns>The retrieved result.</returns>
     public Task<Result> GetResultAsync(long resultId);
+
+    /// <summary>
+    /// Updates an existing result asynchronously.
+    /// </summary>
+    /// <param name="result">The result to update.</param>
+    /// <returns>The updated result.</returns>
     public Task<Result> UpdateResultAsync(Result result);
+
+    /// <summary>
+    /// Deletes a result by its ID asynchronously.
+    /// </summary>
+    /// <param name="resultId">The ID of the result to delete.</param>
+    /// <returns>The deleted result.</returns>
     public Task<Result> DeleteResultAsync(long resultId);
 }
